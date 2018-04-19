eb290c93db566daae66fd034ec2c1f7be41b1b19
user <user@MacBook-Pro.local>
2018-04-18 23:03:30 -0700

    D improve sketch
    sketch plus first Desk test

--- sketch.js
+++ sketch.js
@@ -1,1 +1,20 @@
 //new
+
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols)
+
+function setup() {
+  createCanvas(400,400);
+  console.log("a star");
+
+  for (var i = 0; i < cols; i++) {
+    grid[i] = new Array(rows);
+  }
+  console.log(grid);
+}
+
+
+function draw() {
+  background(0);
+}
